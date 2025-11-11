162a198d5dc69fa2f628dbf42dff67d60efd5729
nderumier <nathan.derumier@student.uclouvain.be>
2025-11-11 16:42:37 +0100

    test

--- telecom/python/telecom/chain.py
+++ telecom/python/telecom/chain.py
@@ -5,7 +5,7 @@ BIT_RATE = 50e3
 PREAMBLE = np.array([int(bit) for bit in f"{0xAAAAAAAA:0>32b}"])
 SYNC_WORD = np.array([int(bit) for bit in f"{0x3E2A54B7:0>32b}"])
 
-
+#test
 class Chain:
     name: str = ""
 
@@ -172,16 +172,6 @@ class BasicChain(Chain):
     ideal_cfo_estimation = False
 
     def cfo_estimation(self, y):
-<<<<<<< HEAD
-        R = self.osr_rx      # Receiver oversampling factor
-        N = 4                # Number of CPFSK symbols per block, /!\ N=2 
-        Nt = N * R           # Number of samples per block
-        Ts = 1 / self.bit_rate  # Symbol duration
-
-        y1 = y[0:Nt]
-        y2 = y[Nt:2*Nt]
-
-=======
         """
         Estimates CFO using Moose algorithm, on first samples of preamble.
         """
@@ -196,17 +186,13 @@ class BasicChain(Chain):
         y2 = y[Nt:2*Nt]
 
         # --- Moose algorithm correlation ---
->>>>>>> a5aef046c08ba75f745ca0fa99b9a0077bcf7bbb
         numerator = np.sum(y2 * np.conj(y1))
         denominator = np.sum(np.abs(y1)**2)
 
         alpha_hat = numerator / denominator
 
-<<<<<<< HEAD
-=======
         # --- CFO estimate ---
         # Frequency offset = phase(alpha_hat) / (2π * Nt * Ts / R)
->>>>>>> a5aef046c08ba75f745ca0fa99b9a0077bcf7bbb
         cfo_est = np.angle(alpha_hat) / (2 * np.pi * Nt * Ts / R)
 
         return cfo_est
