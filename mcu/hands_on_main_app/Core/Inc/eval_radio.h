72a2fc3a794f6d12ee7892ccd34b294319021ef4
Nicolas Van Gorp <nicolas.vangorp@student.uclouvain.be>
2025-12-09 10:24:39 +0100

    Merge upstream/main into main

--- mcu/hands_on_main_app/Core/Inc/eval_radio.h
+++ mcu/hands_on_main_app/Core/Inc/eval_radio.h
@@ -11,7 +11,6 @@
 #define MAX_PA_LEVEL  0  // final Tx transmit power, in dBm
 #define STEP_PA_LEVEL 0  // step for TX transmit increase, in dBm. 0 means MIN_PA_LEVEL is always kept
 #define N_PACKETS 100 // number of packets transmitted for each Tx power level
-
 #define PAYLOAD_LEN 100 // payload length of the transmitted packets
 #define PACKET_DELAY 1000 // delay between two packets, in milliseconds
 
